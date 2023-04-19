58608bb1d8ef82acb5c88e1c2091cf173a5ae9bf
Haruka Ito <itoh.haruka@lmi.ne.jp>
2023-04-19 21:57:22 +0900

    feat: ip restriction

--- middleware.ts
+++ middleware.ts
@@ -4,15 +4,26 @@ export function middleware(req: NextRequest) {
   const basicAuth = req.headers.get('authorization')
   const url = req.nextUrl
 
-  if (basicAuth) {
-    const authValue = basicAuth.split(' ')[1]
-    const [user, pwd] = atob(authValue).split(':')
+  // IP ホワイトリスト検証
+  const ipWhitelist = process.env.IP_WHITELIST?.split(';');
+  const reqIps = req.headers.get('x-forwarded-for')?.split(', ');
+	if (ipWhitelist !== undefined && reqIps !== undefined) {
+    // IP ホワイトリストに含まれていればアクセス許可
+    if (reqIps.some((ip) => ipWhitelist.includes(ip))) {
+      if (basicAuth) {
+        const authValue = basicAuth.split(' ')[1]
+        const [user, pwd] = atob(authValue).split(':')
 
-    if (user === process.env.BASIC_AUTH_USER && pwd === process.env.BASIC_AUTH_PASS) {
-      return NextResponse.next()
+        if (user === process.env.BASIC_AUTH_USER && pwd === process.env.BASIC_AUTH_PASS) {
+          return NextResponse.next()
+        }
+      }
+      url.pathname = '/api/auth'
+
+      return NextResponse.rewrite(url)
     }
   }
-  url.pathname = '/api/auth'
 
-  return NextResponse.rewrite(url)
+  // ステータスは 200 なのであれだが middleware ではそれが手軽にできないので一旦許容
+  return NextResponse.json({ message: 'unauthorized' });
 }
