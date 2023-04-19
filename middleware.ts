12bff4af9ef5b0bdc71b0a919d2239ae14c23208
Haruka Ito <itoh.haruka@lmi.ne.jp>
2023-04-19 21:39:24 +0900

    feat: basic authのuserとpassは環境変数から渡す

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-// export const config = {
-//   matcher: ['/', '/index'],
-// }
-
 export function middleware(req: NextRequest) {
   const basicAuth = req.headers.get('authorization')
   const url = req.nextUrl
@@ -12,7 +8,7 @@ export function middleware(req: NextRequest) {
     const authValue = basicAuth.split(' ')[1]
     const [user, pwd] = atob(authValue).split(':')
 
-    if (user === '4dmin' && pwd === 'testpwd123') {
+    if (user === process.env.BASIC_AUTH_USER && pwd === process.env.BASIC_AUTH_PASS) {
       return NextResponse.next()
     }
   }
